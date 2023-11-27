3a859c01f515fffbb805567139d45e61634d5b0e
Patrik_Jørgensen <vadmand806@gmail.com>
2023-11-27 10:52:59 +0100

    Opgave med .env færdig

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-import express, { application } from 'express';
+import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 4000
+
+
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+const DBUSER = process.env.DBUSER;
+const DBPASSWORD = process.env.DBPASSWORD;
+const DBPORT = process.env.DBPORT;
+
+console.log(`Dette er min host: ${process.env.DBHOST}`);
+console.log(`Dette er Databasens navn: ${process.env.DBNAME}`);
+console.log(`Dette er min user: ${process.env.DBUSER}`);
+console.log(`Dette er mit password: ${process.env.DBPASSWORD}`);
+console.log(`Dette er min database port: ${process.env.DBPORT}`);
+
+
 
 //Router
 app.get('/', (req, res) => {
